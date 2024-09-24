0bf9f14e713fd49b1618e2efbe3fcc83df319440
knurtts <robert.anderson322@outlook.com>
2024-09-23 21:53:35 -0500

    Finally, some content! added stubs to main home menu section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
   <main>
-    <h1>Hello</h1>
+    <div class="flex h-screen w-screen justify-center">
+      <div class="content-center mr-6">
+        <div class="text-center my-20 font-extrabold text-4xl">Robert Anderson</div>
+        <div>
+          <img class="w-96" src="@/assets/images/beard-man.jpg" />
+        </div>
+      </div>
+      <div class="content-center ml-6">
+        <div class="text-3xl">Who do I am are you do you think?</div>
+        <div class="text-3xl">What does it have I done?</div>
+        <div class="text-3xl">Do this want contact?</div>
+        <div class="text-3xl">Does Bruno Mars is gay?</div>
+      </div>
+    </div>
   </main>
 </template>
 
