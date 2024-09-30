34081e40f2873a78453ae0091cf19e56a1fd0cf8
Robert Anderson <robert@Roberts-MacBook-Pro.local>
2024-09-29 21:21:52 -0500

    stub out some content for my work section. uhg.. writing is hard

--- src/components/MyWork.vue
+++ src/components/MyWork.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="flex h-screen max-w-[1280px] mx-auto justify-center">
+  <div class="h-screen max-w-[1280px] mx-auto justify-center">
     I will be putting my work history and projects here
+    <div>
+      <ul>
+        <li>Deleting Code is fun! Refactoring</li>
+        <li>Building Full-Stack apps with automation: Invoicing App</li>
+        <li>Writing performant and maintainable production code: AdGem Offerwall</li>
+      </ul>
+    </div>
   </div>
 </template>
 
