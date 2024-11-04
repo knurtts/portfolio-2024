67a24407997ba6ce871c4de0f129ad9931d9f55f
Robert Anderson <robert@Roberts-MacBook-Pro.local>
2024-11-04 10:16:48 -0600

    add SpookyEye component

---
+++ src/components/common/SpookyEye.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    ref="eye"
+    :style="eyeCss"
+    class="relative inline-block rounded-full h-8 w-8 bg-slate-200 after:absolute after:bottom-4 after:right-2 after:w-3 after:h-3 after:bg-black after:rounded-full"
+  />
+</template>
+
+<script lang="ts"></script>
